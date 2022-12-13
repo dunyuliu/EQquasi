b09199a155d54a97930dfc14355aa33f7e5f965a
DUNYU LIU <dunyuliu@gmail.com>
2022-12-13 15:18:32 -0600

    Add case liu2020-planar

--- case_input/liu2020-planar/user_defined_params.py
+++ case_input/liu2020-planar/user_defined_params.py
@@ -2,33 +2,32 @@
 
 import numpy as np
 from math import *
+from lib     import *
 
 # cylce id. Simulate quasi-dynamic earthquake cycles from istart to iend.
-# should be 1, 2, 3 ...
-# -999 for mode 2, fully-dynamic
-istart     = -999 
-iend       = -999
+istart     = 1 
+iend       = 3
 # mode of the code - quasi-dynamic (1) or fully-dynamic (2). 
 mode       = 2
 
 # model_domain (in meters)
-xmin, xmax = -500, 500
-ymin, ymax = -500, 500
-zmin, zmax = -500, 500
+xmin, xmax = -30e3, 30e3
+ymin, ymax = -30e3, 30e3
+zmin, zmax = -30e3, 0
 
 # creeping zone bounaries.
 # creeping zones are assinged on the lateral sides and bottom of 
 # the RSF controlled region and will slide at fixed loading slip rate.
-xminc, xmaxc, zminc = -400, 400, -400
+xminc, xmaxc, zminc = -25e3, 25e3, -25e3
 
-dx = 10.0e0 # cell size, spatial resolution
-nuni_y_plus, nuni_y_minus = 5, 5 # along the fault-normal dimension, the number of cells share the dx cell size.
-enlarging_ratio = 1.0 # along the fault-normal dimension (y), cell size will be enlarged at this ratio compoundly.
+dx = 300.0e0 # cell size, spatial resolution
+nuni_y_plus, nuni_y_minus = 10, 10 # along the fault-normal dimension, the number of cells share the dx cell size.
+enlarging_ratio = 1.3 # along the fault-normal dimension (y), cell size will be enlarged at this ratio compoundly.
 
 # Isotropic material propterty.
 # Vp, Vs, Rou
 vp, vs, rou = 6.0e3, 3.464e3, 2.67e3
-init_norm = -25.0e6 # initial normal stress in Pa. Negative compressive.
+init_norm = -50.0e6 # initial normal stress in Pa. Negative compressive.
 
 # Controlling switches for EQquasi system
 rough_fault = 0 # include rough fault yes(1) or not(0).
@@ -37,15 +36,15 @@ friclaw     = 3 # rsf_aging(3), rsf_slip(4).
 ntotft      = 1 # number of total faults.
 solver      = 1 # solver option. MUMPS(1, recommended). AZTEC(2).
 nt_out      = 100 # Every nt_out time steps, disp of the whole model and on-fault variables will be written out in netCDF format.
-bp          = 7 # for description only
+bp          = 1000 # for description only
 # currently supported cases
 # 5 (SCEC-BP5)
 # 7 (SCEC-BP7)
-# 1001 (GM-cycle)
+# 1000 (liu2020-planar, GM-cycle)
 
 # xi, minimum Dc
 xi = 0.05 # xi used to limit variable time step size. See Lapusta et al. (2009).
-minDc = 0.5e-3 # meters
+minDc = 11e-3 # meters
 
 # loading 
 far_vel_load = 5e-10 # far field loading velocity on xz planes. A minus value is applied on the other side.
@@ -76,15 +75,12 @@ def shear_steady_state(a,b,v0,r0,load_rate,norm,slip_rate):
   res = -norm*a*asinh(slip_rate/2.0/v0*exp((r0+b*log(v0/load_rate))/a)) + rou*vs/2.0*slip_rate
   return res
 
-rad = 200 # radius of the velocity weakening zone
 for ix, xcoor in enumerate(fx):
   for iz, zcoor in enumerate(fz):
   # assign a in RSF. a is a 2D distribution.
-    radii = sqrt(xcoor*xcoor + zcoor*zcoor)
-    if radii <= rad:
-      on_fault_vars[ix,iz,9] = fric_rsf_a
-    else: 
-      on_fault_vars[ix,iz,9] = fric_rsf_a + fric_rsf_deltaa
+    tmp1  = B1(xcoor, 20.e3, 3.e3)
+    tmp2  = B2(-zcoor, 20.e3, 3.e3)
+    on_fault_vars[ix,iz,9]  = fric_rsf_a + (1. - tmp1*tmp2)*fric_rsf_deltaa
     on_fault_vars[ix,iz,10] = fric_rsf_b # assign b in RSF 
     on_fault_vars[ix,iz,11] = fric_rsf_Dc # assign Dc in RSF.
     on_fault_vars[ix,iz,12] = fric_rsf_v0 # initial reference slip rate.
@@ -110,7 +106,7 @@ for ix, xcoor in enumerate(fx):
 ####################################
 ##### HPC resource allocation ######
 ####################################
-casename       = "bp7-fd-a-10"
+casename       = "liu2020-planar"
 HPC_nnode      = 1 # Number of computing nodes. On LS6, one node has 128 CPUs.
 HPC_ncpu       = 20 # Number of CPUs requested.
 HPC_queue      = "normal" # q status. Depending on systems, job WALLTIME and Node requested.
@@ -123,14 +119,13 @@ HPC_email      = "dliu@ig.utexas.edu" # Email to receive job status.
 ##############################################
 
 # (x,z) coordinate pairs for on-fault stations (in km).
-st_coor_on_fault  = [[0, 0], [-100,0], [0,100], [100,0], \
-                    [0,-100], [-100,-100], [-100,100], [100,-100], [100,100], \
-                    [-300,0], [0, 300], [300,0], [0,-300]]
-st_coor_on_fault  = np.asarray(st_coor_on_fault)/1000.0
+st_coor_on_fault = [[0.0, -3.0], [0.0,-7.5], [0.0, -12.0], [9.0,-7.5], \
+   [12.0, -3.0], [12.0,-12.0], [15.0, -7.5], [18.0,-7.5], [-9.0,-7.5], \
+   [-12.0,-3.0], [-12.0,-12.0], [-15.0, -7.5], [-18.0, -7.5]]
+   
 # (x,y,z) coordinates for off-fault stations (in km).
-st_coor_off_fault = [[0,200,0], [0,400,0], [-300,400,0], [0,400,-300], [300,400,0], \
-                     [0,400,300]]
-st_coor_off_fault = np.asarray(st_coor_off_fault)/1000.0
+st_coor_off_fault = [[0,9,0], [0,-9,0], [12,6,0], [12,-6,0], [-12,6,0], \
+   [-12,-6,0]]
 
 n_on_fault        = len(st_coor_on_fault)
 n_off_fault       = len(st_coor_off_fault)

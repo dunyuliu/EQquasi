5cfd05360a896bf3fa7de3e7f2df2efb2eabbbc8
DUNYU LIU <dunyuliu@gmail.com>
2024-01-28 00:04:13 +0800

    Update install.mumps.sh for 5.6.2 and add support to utig machine - or generally locally compiled mumps
    Preparing test.bp5.qdc to test system

--- install.mumps.sh
+++ install.mumps.sh
@@ -1,12 +1,12 @@
 #! /bin/bash 
 
 # install mumps 
-echo 'Installing a local copy of mumps ... ...'
+echo 'Installing a local copy of mumps through scivision/mumps ... ...'
 
 rm -rf mumps
 git clone https://github.com/scivision/mumps.git
 cd mumps
-cmake -B build
+cmake -B build -DMUMPS_UPSTREAM_VERSION=5.6.2
 cmake --build build
 cd ..
 

a0830d0e950a430ee6d98a329c248d2993f0f9b8
DUNYU LIU <dunyuliu@gmail.com>
2024-07-23 18:16:11 +0800

    Syntax improvements for makefile and install.mumps.sh

--- install.mumps.sh
+++ install.mumps.sh
@@ -1,13 +1,17 @@
 #! /bin/bash 
 
-# install mumps 
-echo 'Installing a local copy of mumps through scivision/mumps ... ...'
-
+echo 'Installing a local copy of MUMPS through scivision/mumps ...'
+echo 'Deleting old copy ...'
 rm -rf mumps
+
 git clone https://github.com/scivision/mumps.git
 cd mumps
-cmake -B build -DMUMPS_UPSTREAM_VERSION=5.6.2
+cmake -B build # use the latest MUMPS.
+# cmake -B build -DMUMPS_UPSTREAM_VERSION=5.6.2
 cmake --build build
+cmake --install build
 cd ..
 
-echo 'mumps is installed ...'
+echo 'MUMPS should be installed now ...'
+echo 'Libraries are under /mumps/build/local/lib ...'
+echo 'Include files are under /mumps/build/local/include ...'

376a3b5332de0edd07ae005c79c82dcbe1a9a205
DUNYU LIU <dunyuliu@gmail.com>
2024-07-23 18:21:22 +0800

    Use netCDF4 instead of Xarray. Remove 3rd party package dependency.

--- check.test.py
+++ check.test.py
@@ -1,35 +1,54 @@
 
 import os, time
 import numpy as np
-import xarray as xr
+from netCDF4 import Dataset
 from testNameList import nameList
 
 fileNameList = ['fault.00001.nc','fault.00040.nc','global.dat', 'tdyna.txt']
 refRoot  = 'test.reference.results'
 testRoot = 'test'
 
-def compare_nc_files(fn1,fn2,threshold=1e-3):
+def compare_nc_files(fn1, fn2, threshold=1e-3):
+    def compare_variables(var1, var2):
+        if var1.shape != var2.shape:
+            return False
+        if var1.dtype != var2.dtype:
+            return False
+        return (var1[:] == var2[:]).all()
+
+    with Dataset(fn1, 'r') as f1, Dataset(fn2, 'r') as f2:
+        if list(f1.variables.keys()) != list(f2.variables.keys()):
+            isTheSame = 'FAIL '+fn1+' '+fn2
+
+        for key in f1.variables.keys():
+            if not compare_variables(f1.variables[key], f2.variables[key]):
+                isTheSame = 'FAIL '+fn1+' '+fn2
+
     isTheSame = 'SUCCESS '+fn1+' '+fn2
-    f1 = xr.open_dataset(fn1)
-    f2 = xr.open_dataset(fn2)
-    metadata_equal = f1.identical(f2)
-    data_equal = (f1==f2).all().items()
-    
-    # Compare variables
-    for var in f1.variables:
-        var1 = f1[var]
-        var2 = f2[var]
-        if var1.dims != var2.dims:
-            isTheSame = 'FAIL var dim '+fn1+' '+fn2
-        if not np.allclose(var1,var2,rtol=threshold, atol=threshold):
-            isTheSame = 'FAIL var numbers '+fn1+' '+fn2
-        
-    if not metadata_equal:# and data_equal:
-        isTheSame = 'FAIL metadata '+fn1+' '+fn2
-        
     print(isTheSame)
     
-    return isTheSame
+# def compare_nc_files_old(fn1,fn2,threshold=1e-3):
+    # isTheSame = 'SUCCESS '+fn1+' '+fn2
+    # f1 = xr.open_dataset(fn1)
+    # f2 = xr.open_dataset(fn2)
+    # metadata_equal = f1.identical(f2)
+    # data_equal = (f1==f2).all().items()
+    
+    # # Compare variables
+    # for var in f1.variables:
+        # var1 = f1[var]
+        # var2 = f2[var]
+        # if var1.dims != var2.dims:
+            # isTheSame = 'FAIL var dim '+fn1+' '+fn2
+        # if not np.allclose(var1,var2,rtol=threshold, atol=threshold):
+            # isTheSame = 'FAIL var numbers '+fn1+' '+fn2
+        
+    # if not metadata_equal:# and data_equal:
+        # isTheSame = 'FAIL metadata '+fn1+' '+fn2
+        
+    # print(isTheSame)
+    
+    # return isTheSame
 
 def compare_txt_files(fn1,fn2,threshold=1e-3):
     isTheSame = 'SUCCESS '+fn1+' '+fn2

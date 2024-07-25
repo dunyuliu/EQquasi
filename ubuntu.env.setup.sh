8f2ca9b2654e104a5ad44533d50d38f849693497
DUNYU LIU <43709243+dunyuliu@users.noreply.github.com>
2024-07-25 18:39:59 +0800

    Update ubuntu.env.setup.sh

--- ubuntu.env.setup.sh
+++ ubuntu.env.setup.sh
@@ -4,7 +4,7 @@
 #   necessary packages for EQquasi on Ubuntu.
 apt-get update
 apt-get install git vim openmpi-bin make cmake
-apt-get install python3 python3-pip python3-netcdf4 
+apt-get install python3 python3-pip  
 apt-get install libmumps-dev python3-matplotlib
 apt-get install libnetcdf-dev libnetcdff-dev
-pip install xarray imageio pdf2image numpy==1.26.4
+pip install xarray imageio pdf2image numpy==1.26.4 netCDF4
